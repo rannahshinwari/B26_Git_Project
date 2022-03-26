4cde9a33d5a04135b8473b12137c61e7fd1b92ae
rannahshinwari <Rannahshinwari01@gmail.com>
2022-03-26 16:14:34 -0400

    First java

---
+++ src/day_0/First.java
@@ -0,0 +1,15 @@
+package day_0;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("Working...");
+    }
+
+
+    // extra line 1
+    // extra line 2
+    // extra line 3
+}
